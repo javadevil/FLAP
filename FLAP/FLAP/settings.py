0667beb778c55f6ee851ed2fa5c228fd17d786f9
Wacharabuhm <n.kimmy@gmail.com>
2013-10-26 08:32:01 +0700

    Change db setting

--- FLAP/FLAP/settings.py
+++ FLAP/FLAP/settings.py
@@ -15,9 +15,9 @@ DATABASES = {
         'NAME': 'FLAP',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5433',                      # Set to empty string for default.
     }
 }
 
